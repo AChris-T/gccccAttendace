c50eb0d91acfb522b6131c26dd96e2fdb18a9801
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2025-10-21 13:17:03 +0100

    feat: auth completed

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -15,28 +15,33 @@ const Navbar = () => {
     <>
       <div className='fixed top-0 right-0 left-0 z-50 container mx-auto'>
         <div className="flex w-full  items-center z-50 relative  justify-between md:items-center px-4">
-          <Link to='/' className='hidden md:flex'>
-            <img
-              src='/images/logo/logo-white.png'
-              alt="menu"
-              className="w-[200px] h-[80px] object-contain"
-            /></Link>
-          <Link to='/' className='md:hidden'>
-            <img
-              src='/images/logo/gccc.png'
-              alt="menu"
-              className="flex w-[80px] h-[80px] object-cover"
-            />
-          </Link>
+          <div>
 
-          <p className="text-[14px] flex items-center gap-2 text-white leading-6 font-normal">
-            {formattedTime} |{' '}
-            <span className="text-[#86888A]">
-              {formattedDateTime}
-            </span>{' '}
-          </p>
-        </div>
-        <div className="z-50 max-w-[1940px] mx-auto w-full flex justify-between lg:px-[142px] px-4 pt-3 pb-4">
+            <Link to='/' className='hidden md:flex'>
+              <img
+                src='/images/logo/logo-white.png'
+                alt="menu"
+                className="w-[200px] h-[80px] object-contain"
+              />
+            </Link>
+            <Link to='/' className='md:hidden'>
+              <img
+                src='/images/logo/gccc.png'
+                alt="menu"
+                className="flex w-[80px] h-[80px] object-cover"
+              />
+            </Link>
+
+            <p className="text-[14px] flex items-center gap-2 text-white leading-6 font-normal">
+              {formattedTime} |{' '}
+              <span className="text-[#86888A]">
+                {formattedDateTime}
+              </span>{' '}
+            </p>
+          </div>
+
+
+
           <NavLink
             to="/"
             className={({ isActive }) =>
